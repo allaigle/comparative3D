75aeafc40614bb56a9cdfbbda6d336550b15e72d
Alice LAIGLE <89134890+allaigle@users.noreply.github.com>
2026-02-05 11:33:06 +0100

    Update C2_run_busco_TBaril.sh

--- C2_run_busco_TBaril.sh
+++ C2_run_busco_TBaril.sh
@@ -6,17 +6,17 @@
 #SBATCH --time=168:00:00 # Each task takes max 7 days
 #SBATCH --mem-per-cpu=8G # Each task uses max 8GB memory
 #SBATCH --cpus-per-task=2 # Each task uses 2 CPUs
-#SBATCH --array=1-21%10 # submit 42 tasks with task ID 1,2,...,21.
+#SBATCH --array=1-63%10 # submit 63 tasks with task ID 1,2,...,63.
 
 # set directories
 data_dir=/data/alicel/chapter2/1_dataTreatment/2_phyloTree/0_inputs/genomes
 
 # get parameters - this is a file with the name of each genome file, and the 
 ## name of the resultant output file (WITHOUT PATHS)
-param_store=/data/alicel/chapter2/1_dataTreatment/2_phyloTree/1_busco/buscoParameters.21.txt
+param_store=/data/alicel/chapter2/1_dataTreatment/2_phyloTree/1_busco/buscoParameters.63.txt
 ##### param_store contains tab-separated lines that look like #####
 ##### agabis3.fna	BUSCO_agabis3 #####
-### this file contains 42 lines with 2 arguments per line
+### this file contains 63 lines with 2 arguments per line
 ### line <i> contains arguments for run <i>
 # get first argument (name of genome file)
 arg_one=$(cat $param_store | awk -v var=$SLURM_ARRAY_TASK_ID 'NR==var {print $1}')
